a0946966d32169c3230aa16507a84101f5b6206d
smkilani <s.kilani@ucl.ac.uk>
2015-10-01 16:27:11 +0100

    Added duplex LV control

--- Firmware/ATMEGA2560/master/master.ino
+++ Firmware/ATMEGA2560/master/master.ino
@@ -18,11 +18,12 @@ char inputString[50];
 
 //const PROGMEM  String menu[6]  = {"on\r","off\r","status\r","sc\r","version\r","help\r"};
 
-String menu[6] = {"on","off","status","sc","version","help"};
+String menu[10] = {"ona","onb","offa","offb","statusa","statusb","sca","scb","version","help"};
 //String adrs_rsp = "Enter LV board address";
 String reply[2] = {"OK","Error"};
+int selected_menu=-1;
 int slave_adrs=0;
-bool sc_mode = false;
+bool sc_mode = false;;
 bool stringComplete = false;  // whether the string is complete
 byte x=0;
 int y=0;
@@ -37,23 +38,97 @@ void setup()
 void loop()
 {
   
-  serialEvent(); //This is only needed for the Arduino micro
+  //serialEvent(); //This is only needed for the Arduino micro
     // print the string when a newline arrives:
     
   if (stringComplete) {
   if (y>1) { //to check if the command is not empty
-
+      selected_menu=-1;
+      for (int i=0;i<10;i++){
+        if (((String)inputString).startsWith(menu[i])) selected_menu=i;
+      }
+      slave_adrs=((String)inputString).substring((menu[selected_menu].length())+1,((String)inputString).length()).toInt();
       //Serial.println(inputString);
       //Serial.println( menu[0]);
+
+
+
+      switch (selected_menu){
+        case 0:
+          Wire.beginTransmission(slave_adrs); // transmit to device #
+          Wire.write(0x02);              // sends one byte
+          if (Wire.endTransmission()==0) Serial.println(reply[0]);  // stop transmitting
+          else Serial.println(reply[1]);
+        break;
+        case 1:
+          Wire.beginTransmission(slave_adrs); // transmit to device #
+          Wire.write(0x02);              // sends one byte
+          if (Wire.endTransmission()==0) Serial.println(reply[0]);  // stop transmitting
+          else Serial.println(reply[1]);
+        break;
+        case 2:
+          Wire.beginTransmission(slave_adrs); // transmit to device #
+          Wire.write(0x01);              // sends one byte
+          if (Wire.endTransmission()==0) Serial.println(reply[0]);  // stop transmitting
+          else Serial.println(reply[1]);
+        break;
+        case 3:
+          Wire.beginTransmission(slave_adrs); // transmit to device #
+          Wire.write(0x01);              // sends one byte
+          if (Wire.endTransmission()==0) Serial.println(reply[0]);  // stop transmitting
+          else Serial.println(reply[1]);
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:
+          Wire.beginTransmission(slave_adrs); // transmit to device #
+          Wire.write(0x03);              // sends one byte
+          if (Wire.endTransmission()==0) {
+            Serial.print("SC on device ");
+            Serial.print(slave_adrs);
+            Serial.println(" A");
+            sc_mode=true;    // stop transmitting
+          }
+          else {
+            Serial.println("Error");
+            sc_mode=false; 
+          }
+        break;
+        case 7:
+          Wire.beginTransmission(slave_adrs); // transmit to device #
+          Wire.write(0x03);              // sends one byte
+          if (Wire.endTransmission()==0) {
+            Serial.print("SC on device ");
+            Serial.print(slave_adrs);
+            Serial.println(" B");
+            sc_mode=true;    // stop transmitting
+          }
+          else {
+            Serial.println("Error");
+            sc_mode=false; 
+          }
+        break;
+        case 8:
+        Serial.println(ver);
+        break;
+        case 9:
+        for (int i = 0;i<10;i++) Serial.println(menu[i]);
+        break;
+        default: 
+        Serial.println("Bad command");   
+      }
+      /*
         if (((String)inputString).startsWith(menu[0])) {
-          slave_adrs=((String)inputString).substring(3,((String)inputString).length()).toInt();
+          //slave_adrs=((String)inputString).substring((menu[0].length())+1,((String)inputString).length()).toInt(); //generalize this
           Wire.beginTransmission(slave_adrs); // transmit to device #
           Wire.write(0x02);              // sends one byte
           if (Wire.endTransmission()==0) Serial.println(reply[0]);  // stop transmitting
           else Serial.println(reply[1]);
         }
         else if (((String)inputString).startsWith(menu[1])) {
-          slave_adrs=((String)inputString).substring(4,((String)inputString).length()).toInt();
+          //slave_adrs=((String)inputString).substring(4,((String)inputString).length()).toInt();
           Wire.beginTransmission(slave_adrs); // transmit to device #
           Wire.write(0x01);              // sends one byte
           if (Wire.endTransmission()==0) Serial.println(reply[0]);  // stop transmitting
@@ -61,7 +136,7 @@ void loop()
         }  
         else if (((String)inputString).startsWith(menu[2])) {
             //Serial.write("\nPolling status\n");
-            slave_adrs=((String)inputString).substring(7,((String)inputString).length()).toInt();
+            //slave_adrs=((String)inputString).substring(7,((String)inputString).length()).toInt();
             byte datain[7];
             int i=0;
             if (Wire.requestFrom(slave_adrs, 7)<7) Serial.println("Error");    // request 3 byte from slave device #
@@ -105,7 +180,7 @@ void loop()
             }
         }
         else if (((String)inputString).startsWith(menu[3])) {
-            slave_adrs=((String)inputString).substring(3,((String)inputString).length()).toInt();
+            //slave_adrs=((String)inputString).substring(3,((String)inputString).length()).toInt();
             Wire.beginTransmission(slave_adrs); // transmit to device #
             Wire.write(0x03);              // sends one byte
             if (Wire.endTransmission()==0) {
@@ -130,6 +205,7 @@ void loop()
         else {
           Serial.println("Bad command");
         }
+    */
     }
     
     
