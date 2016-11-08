9d81fda55201a220cce5a0f8c2999781926d9e93
Samer Kilani <smkilani@gmail.com>
2016-11-08 14:20:27 +0000

    acdc firmware

--- Firmware/ACDCcontroller/ACDCcontroller/ACDCcontroller.ino
+++ Firmware/ACDCcontroller/ACDCcontroller/ACDCcontroller.ino
@@ -13,6 +13,7 @@
 #include <Wire.h>
 #include <TFT.h>  // Arduino LCD library
 #include <SPI.h>
+#include <assert.h>
 
 // pin definition for the Uno
 #define cs   10
@@ -27,6 +28,8 @@ float new_voltage=0;
 float old_current2=-2;
 float old_voltage2=-2;
 float new_current=0;
+bool i2cstatus1=false;
+bool i2cstatus2=false;
 
 char sensorPrintout1[5];
 char sensorPrintout2[5];
@@ -69,8 +72,9 @@ void setup() {
 
 void loop() {
   //delay(2000);
-
+//Serial.println(linear(0xba,0x00));
 readPSUout();
+
 delay(1000);
 
 
@@ -90,71 +94,83 @@ delay(1000);
 
 void readPSUout()
 {
-    new_current=readCurrent(0x59);
-    delay(50);
-    new_voltage=readVoltage(0x59);
+    
+    bool alive=false;
+  
+    
+    
+    
+    readVoltage(0x59,&alive,&new_voltage);
+    
     delay(50);
     
   if (new_voltage!=old_voltage1) {
     // erase the text you just wrote
     TFTscreen.stroke(0, 0, 0);
     TFTscreen.setCursor(10,30);
-    if (old_voltage1>-1) TFTscreen.print(old_voltage1); else TFTscreen.print("Error");
+    if (alive==true) TFTscreen.print(old_voltage1); else TFTscreen.print("Error");
     
     //Serial.println();
     TFTscreen.stroke(255, 255, 255);
     // print the sensor value
     TFTscreen.setCursor(10,30);
-    if (new_voltage>-1) TFTscreen.print(new_voltage); else TFTscreen.print("Error");
+    if (alive==true) TFTscreen.print(new_voltage); else TFTscreen.print("Error");
     old_voltage1=new_voltage;
   }
+
+  readCurrent(0x59,&alive,&new_current);
+  delay(50);
+  
   if (new_current!=old_current1) {
     //Serial.println();
     // erase the text you just wrote
     TFTscreen.stroke(0, 0, 0);
     TFTscreen.setCursor(10,90);
-    //if (old_current1>-1) TFTscreen.print(old_current1); else TFTscreen.print("Error");
-    TFTscreen.print(old_current1);
+    if (alive==true) TFTscreen.print(old_current1); else TFTscreen.print("Error");
+    //TFTscreen.print(old_current1);
     TFTscreen.stroke(255, 255, 255);
     // print the sensor value
     TFTscreen.setCursor(10,90);
-    //if (new_current>-1) TFTscreen.print(new_current); else TFTscreen.print("Error");
-    TFTscreen.print(new_current);
+    if (alive==true) TFTscreen.print(new_current); else TFTscreen.print("Error");
+    //TFTscreen.print(new_current);
     old_current1=new_current;
   }
 
 
 
-    new_current=readCurrent(0x58);
-    delay(50);
-    new_voltage=readVoltage(0x58);
+
+    readVoltage(0x58,&alive,&new_voltage);
     delay(50);
     
   if (new_voltage!=old_voltage2) {
     // erase the text you just wrote
     TFTscreen.stroke(0, 0, 0);
     TFTscreen.setCursor(90,30);
-    if (old_voltage2>-1) TFTscreen.print(old_voltage2); else TFTscreen.print("Error");
+    if (alive==true) TFTscreen.print(old_voltage2); else TFTscreen.print("Error");
     
     //Serial.println();
     TFTscreen.stroke(255, 255, 255);
     // print the sensor value
     TFTscreen.setCursor(90,30);
-    if (new_voltage>-1) TFTscreen.print(new_voltage); else TFTscreen.print("Error");
+    if (alive==true) TFTscreen.print(new_voltage); else TFTscreen.print("Error");
     old_voltage2=new_voltage;
   }
+
+    readCurrent(0x58,&alive,&new_current);
+    delay(50);
+      
   if (new_current!=old_current2) {
     //Serial.println();
     // erase the text you just wrote
     TFTscreen.stroke(0, 0, 0);
     TFTscreen.setCursor(90,90);
-    //if (old_current2>-1) TFTscreen.print(old_current2); else TFTscreen.print("Error");
-    TFTscreen.print(old_current2);
+    if (alive==true) TFTscreen.print(old_current2); else TFTscreen.print("Error");
+    //TFTscreen.print(old_current2);
     TFTscreen.stroke(255, 255, 255);
     // print the sensor value
     TFTscreen.setCursor(90,90);
-    //if (new_current>-1) TFTscreen.print(new_current); else TFTscreen.print("Error");
-    TFTscreen.print(new_current);
+    if (alive==true) TFTscreen.print(new_current); else TFTscreen.print("Error");
+    //TFTscreen.print(new_current);
     old_current2=new_current;
   }
 
@@ -162,19 +178,27 @@ void readPSUout()
 }
 
 
-float readVoltage(byte adrs)
+void readVoltage(byte adrs, bool* alive, float* value)
 {
-  int data=0;
-  float out=0;
+  int data[2]={0,0};
+
+  assert(alive);
+  assert(value);
+
+
+  
   Wire.beginTransmission(adrs); // start transmission
   Wire.write(0x8B); // send command
   if (Wire.endTransmission(false)>0) {
-    Serial.println("Error"); // don't send a stop condition
-    return -1;
+    Serial.println("Error"); 
+    *alive=false;
+    return;
   }
+  *alive=true;
+  
   int l=Wire.requestFrom(adrs, (uint8_t)2); // request expected number of bytes to be returned
   //delay(500);
-  Serial.println("request done!");
+  Serial.println("Voltage:");
   int i=0;
   while(l>0 && Wire.available()) // loop all bytes if any are available
   {
@@ -182,26 +206,37 @@ float readVoltage(byte adrs)
     int temp= Wire.read();
     Serial.print(temp,HEX); // print em out
     Serial.print(" ");
-    data = data + (temp<<(8*i));
+    data[i] = temp;//(temp<<(8*i));
     i++;
   }
-  out=(28.58/14638)*data;
-  return out;
+  Serial.println();
+  //out=(28.58/14638)*data;
+  *value=linear(data[1],data[0]);
+  //return out;
 }
 
-float readCurrent(byte adrs)
+float readCurrent(byte adrs, bool* alive, float* value)
 {
-  int data=0;
-  float out=0;
+  char data[2]={0,0};
+
+
+  
+  assert(alive);
+  assert(value);
+
+  
   Wire.beginTransmission(adrs); // start transmission
   Wire.write(0x8C); // send command
   if (Wire.endTransmission(false)>0) {
     Serial.println("Error"); // don't send a stop condition
-    return 0;//change back to -1 if needed
+    *alive=false;
+    return;//change back to -1 if needed
   }
+  *alive=true;
+  
   int l=Wire.requestFrom(adrs, (uint8_t)2); // request expected number of bytes to be returned
   //delay(500);
-  //Serial.println("request done!");
+  Serial.println("Current:");
   int i=0;
   while(l>0 && Wire.available()) // loop all bytes if any are available
   {
@@ -209,11 +244,46 @@ float readCurrent(byte adrs)
     int temp= Wire.read();
     Serial.print(temp,HEX); // print em out
     Serial.print(" ");
-    data = data + (temp<<(8*i));
+    data[i] = temp;
     i++;
   }
-  out=(28.58/14638)*data;
-  return out;
+  Serial.println();
+  *value=linear(data[1],data[0]);
+}
+
+float linear(unsigned char byte1, unsigned char byte2)
+{
+  int exponent=((byte1)&0xf8)>>3;
+
+  if ((exponent&0x10)==0x10) {
+    exponent=(~exponent&0x1f);
+    //printf("1=%d\n",exponent);
+    //two's complement to one's complement
+    exponent++;
+    //check if negative
+    exponent=-1*(exponent);
+  }
+  
+
+  
+  //joing the 11bit mantissa
+  int mantissa=byte2+(((byte1)&0x07)<<8);
+  //printf("1=%d\n",mantissa);
+  //two's complement to one's complement
+  if ((mantissa&0x400)==0x400) {
+    mantissa=(mantissa)&0x7ff;
+    mantissa--;
+    mantissa=(~mantissa)&0x3ff;
+    //printf("3=%d\n",mantissa);
+    mantissa=-1*(mantissa&0x7ff);
+  }
+
+
+  //printf("exponent = %d\n",exponent);
+  //printf("mantissa = %d\n",mantissa);
+
+  return mantissa*pow(2,exponent);
+  
 }
 
 void test()
